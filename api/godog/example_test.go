1422c7ce60fb195875277ba2f489eeb4c759f6e3
Rickard Englund <rickard.englund@skf.com>
2020-01-17 14:28:35 +0100

    add executeInvalidRequest

--- api/godog/example_test.go
+++ api/godog/example_test.go
@@ -1,33 +1,52 @@
 package godog
 
 import (
+	"fmt"
 	"net/http"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	json_matcher "github.com/SKF/go-tests-utility/api/godog/json"
 )
 
 func TestGetRequest(t *testing.T) {
 	api := BaseFeature{}
-	api.SetBaseUrl("http://dummy.restapiexample.com/")
+	api.SetBaseUrl("https://jsonplaceholder.typicode.com")
 
-	err := api.CreatePathRequest(http.MethodGet, "/api/v1/employee/{id}")
-	assert.NoError(t, err)
+	err := api.CreatePathRequest(http.MethodGet, "/todos/{id}")
+	require.NoError(t, err)
 
 	id := "1"
 	err = api.SetsRequestPathParameterTo("id", id)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 
 
 	err = api.ExecuteTheRequest()
-	assert.NoError(t, err)
+	require.NoError(t, err)
 
 	err = api.AssertResponseCode(http.StatusOK)
 	assert.NoError(t, err)
 
+
 	res, err := json_matcher.Read(api.Response.Body, ".id")
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	assert.Equal(t, id, res)
+}
+
+func TestCreateInvalidRequest(t *testing.T) {
+	api := BaseFeature{}
+	api.SetBaseUrl("https://jsonplaceholder.typicode.com")
+
+	err :=	api.CreatePathRequest(http.MethodPost, "/posts")
+	require.NoError(t, err)
+
+	err = api.ExecuteInvalidRequest()
+	require.NoError(t, err)
+
+	fmt.Printf("%s\n", api.Response.Body)
+
+	err = api.AssertResponseCode(http.StatusInternalServerError)
+	require.NoError(t, err)
 }
