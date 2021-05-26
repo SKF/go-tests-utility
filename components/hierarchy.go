d7ea1cc648c90f11f34b64989e2f349215adfc03
Rickard Englund <rickard.englund@cygni.se>
2021-05-26 15:46:37 +0200

    create components (#29)

---
+++ components/hierarchy.go
@@ -0,0 +1,74 @@
+package components
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/SKF/go-rest-utility/client"
+	"github.com/SKF/go-rest-utility/client/auth"
+	"github.com/SKF/go-utility/v2/log"
+	"github.com/pkg/errors"
+)
+
+const (
+	hierarchyBaseURL = "https://api.%s.hierarchy.enlight.skf.com"
+)
+
+func httpClient(stage, identityToken string) *client.Client {
+	return client.NewClient(
+		client.WithBaseURL(fmt.Sprintf(hierarchyBaseURL, stage)),
+		client.WithDatadogTracing(),
+		client.WithTokenProvider(auth.RawToken(identityToken)),
+	)
+}
+
+func Create(identityToken, stage, parentNodeID, nodetype string) (Component, error) {
+	return CreateWithContext(context.Background(), identityToken, stage, parentNodeID, nodetype)
+}
+
+func CreateWithContext(ctx context.Context, identityToken, stage, parentNodeID, nodetype string) (Component, error) {
+	requestBody := struct {
+		Type     string `json:"type"`
+		Position int    `json:"position"`
+	}{
+		Type:     nodetype,
+		Position: 1,
+	}
+	log.WithTracing(ctx).
+		WithField("body", requestBody).
+		WithField("assetID", parentNodeID).
+		Debugf("creating component")
+
+	req := client.Post(fmt.Sprintf("/assets/%s/components", parentNodeID)).
+		WithJSONPayload(requestBody)
+
+	restClient := httpClient(stage, identityToken)
+	resp, err := restClient.Do(ctx, req)
+	if err != nil {
+		err = errors.Wrap(err, "failed to execute request")
+		return Component{}, err
+	}
+
+	var responseBody struct {
+		Component Component `json:"component"`
+	}
+	if err = resp.Unmarshal(&responseBody); err != nil {
+		err = errors.Wrap(err, "failed to unmarshal response")
+		return Component{}, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		err = errors.Errorf("wrong response status: %q", resp.Status)
+		return Component{}, err
+	}
+
+	return responseBody.Component, nil
+}
+
+type Component struct {
+	ID         string `json:"id"`
+	Type       string `json:"type"`
+	AttachedTo string `json:"attachedToo"`
+	Position   int    `json:"position"`
+}
