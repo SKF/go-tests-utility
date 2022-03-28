2082f6bf2b60d3acf662738707e0541a29e78a68
Erik Karlsson <erik.karlsson@knowit.se>
2022-03-28 12:59:39 +0200

    Revert removal of public func RemoveUserRoleWothContext

--- users/user_permission.go
+++ users/user_permission.go
@@ -24,6 +24,10 @@ func RemoveUserRole(identityToken, stage, userID, role string) error {
 	return updateRoleToAllUsersNodes(context.Background(), identityToken, stage, userID, role, removeRole)
 }
 
+func RemoveUserRoleWithContext(ctx context.Context, identityToken, stage, userID, roleToBeRemoved string) (err error) {
+	return updateRoleToAllUsersNodes(ctx, identityToken, stage, userID, role, removeRole)
+}
+
 func updateRoleToAllUsersNodes(ctx context.Context, identityToken, stage string, userID string, role string, roleFunc updateRoleFunc) error {
 	getNodesRequest := client.Get("/users/{id}/nodes-only").
 		Assign("id", userID)
