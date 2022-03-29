f6191a8c6a6888ad4b725fd4317ec73d90612159
karlssonerik <38431376+karlssonerik@users.noreply.github.com>
2022-03-29 08:54:45 +0200

    Replaced apiweb url to accessapi, reworked user_permission.go (#37)
    * Replaced apiweb url to accessapi, reworked user_permission.go

--- users/user_permission.go
+++ users/user_permission.go
@@ -2,92 +2,38 @@ package users
 
 import (
 	"context"
-	"fmt"
+	"encoding/json"
 	"net/http"
 
 	"github.com/SKF/go-rest-utility/client"
-	"github.com/go-http-utils/headers"
+	"github.com/SKF/go-utility/v2/array"
 	"github.com/pkg/errors"
 )
+
+type updateRoleFunc func(roles []string, roleToUpdate string) []string
 
 func AddUserRole(identityToken, stage, userID, role string) error {
 	return AddUserRoleWithContext(context.Background(), identityToken, stage, userID, role)
 }
 
 func AddUserRoleWithContext(ctx context.Context, identityToken, stage, userID, role string) (err error) {
-	user, err := getUser(ctx, identityToken, stage, userID)
-	if err != nil {
-		return
-	}
-
-	user.UserRoles = append(user.UserRoles, role)
-	return updateUser(ctx, identityToken, stage, user)
+	return updateRoleToAllUsersNodes(ctx, identityToken, stage, userID, role, addRole)
 }
 
 func RemoveUserRole(identityToken, stage, userID, role string) error {
-	return RemoveUserRoleWithContext(context.Background(), identityToken, stage, userID, role)
+	return updateRoleToAllUsersNodes(context.Background(), identityToken, stage, userID, role, removeRole)
 }
 
 func RemoveUserRoleWithContext(ctx context.Context, identityToken, stage, userID, roleToBeRemoved string) (err error) {
-	user, err := getUser(ctx, identityToken, stage, userID)
-	if err != nil {
-		return
-	}
-
-	var newUserRoles = make([]string, 0, len(user.UserRoles))
-	for _, role := range user.UserRoles {
-		if role == roleToBeRemoved {
-			continue
-		}
-
-		newUserRoles = append(newUserRoles, role)
-	}
-
-	if len(newUserRoles) == len(user.UserRoles) {
-		// Nothing to update
-		return
-	}
-
-	user.UserRoles = newUserRoles
-	return updateUser(ctx, identityToken, stage, user)
+	return updateRoleToAllUsersNodes(ctx, identityToken, stage, userID, roleToBeRemoved, removeRole)
 }
 
-func getUser(ctx context.Context, identityToken, stage, userID string) (user user, err error) {
-	if userID == "" {
-		return user, fmt.Errorf("userID is required")
-	}
-
-	req := client.Get("/users/{id}").
-		Assign("id", userID).
-		SetHeader(headers.ContentType, "application/json")
+func updateRoleToAllUsersNodes(ctx context.Context, identityToken, stage string, userID string, role string, roleFunc updateRoleFunc) error {
+	getNodesRequest := client.Get("/users/{id}/nodes-only").
+		Assign("id", userID)
 
 	restClient := httpClientAccessMgmt(stage, identityToken)
-	resp, err := restClient.Do(ctx, req)
-	if err != nil {
-		err = errors.Wrap(err, "failed to execute request")
-		return
-	}
-
-	if resp.StatusCode != http.StatusOK {
-		err = errors.Errorf("wrong response status: %q", resp.Status)
-		return
-	}
-
-	if err = resp.Unmarshal(&user); err != nil {
-		err = errors.Wrap(err, "failed to unmarshal body")
-		return
-	}
-
-	return user, err
-}
-
-func updateUser(ctx context.Context, identityToken, stage string, user user) error {
-	req := client.Put("/users/{id}").
-		Assign("id", user.ID).
-		WithJSONPayload(user)
-
-	restClient := httpClientAccessMgmt(stage, identityToken)
-	resp, err := restClient.Do(ctx, req)
+	resp, err := restClient.Do(ctx, getNodesRequest)
 	if err != nil {
 		return errors.Wrap(err, "failed to execute request")
 	}
@@ -96,21 +42,66 @@ func updateUser(ctx context.Context, identityToken, stage string, user user) err
 		return errors.Errorf("wrong response status: %q", resp.Status)
 	}
 
+	gunhr := getUserNodesHierarchiesResponse{}
+	if err := json.NewDecoder(resp.Body).Decode(&gunhr); err != nil {
+		return errors.Wrap(err, "failed to decode response")
+	}
+
+	for _, node := range gunhr.Data.Nodes {
+
+		req := roleRequest{
+			Roles: roleFunc(node.Roles, role),
+		}
+
+		putRolesRequest := client.Put("/users/{id}/nodes/{nodeId}").
+			Assign("id", userID).
+			Assign("nodeId", node.ID).
+			WithJSONPayload(req)
+
+		resp, err := restClient.Do(ctx, putRolesRequest)
+		if err != nil {
+			return errors.Wrap(err, "failed to execute request")
+		}
+
+		if resp.StatusCode != http.StatusAccepted {
+			return errors.Errorf("wrong response status: %q", resp.Status)
+		}
+	}
+
 	return nil
 }
 
-type user struct {
-	ID             string   `json:"id"`
-	Email          string   `json:"email"`
-	CompanyID      string   `json:"companyId"`
-	UserRoles      []string `json:"userRoles"`
-	Username       string   `json:"username"`
-	UserStatus     string   `json:"userStatus"`
-	EulaAgreedDate string   `json:"eulaAgreedDate"`
-	ValidEula      bool     `json:"validEula"`
-	Firstname      string   `json:"firstname"`
-	Surname        string   `json:"surname"`
-	Locale         string   `json:"locale"`
-	CreatedDate    string   `json:"createdDate"`
-	UpdatedDate    string   `json:"updatedDate"`
+func addRole(roles []string, newRole string) []string {
+	if array.ContainsString(roles, newRole) {
+		return roles
+	}
+	return append(roles, newRole)
+}
+
+func removeRole(roles []string, roleToBeRemoved string) []string {
+	var newUserRoles = make([]string, 0, len(roles))
+	for _, role := range roles {
+		if role == roleToBeRemoved {
+			continue
+		}
+
+		newUserRoles = append(newUserRoles, role)
+	}
+	return newUserRoles
+}
+
+type roleRequest struct {
+	Roles []string
+}
+
+type getUserNodesHierarchiesResponse struct {
+	Data nodeHierarchies `json:"data"`
+}
+
+type nodeHierarchies struct {
+	Nodes []nodeHierarchy `json:"nodes"`
+}
+type nodeHierarchy struct {
+	ID    string   `json:"id"`
+	Roles []string `json:"roles"`
 }
