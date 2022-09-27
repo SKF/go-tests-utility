b909b82d499347b9a186d3a73d2bab09c1fbaeac
Youssef khaya <youssef.khaya@skf.com>
2022-09-27 15:58:52 +0200

    fix regex and rules on passwrod

--- users/create_user_test.go
+++ users/create_user_test.go
@@ -26,15 +26,6 @@ func TestGetTemporaryPassword_NotEmptyPassword(t *testing.T) {
 	require.NotNil(t, err)
 }
 
-func TestGetTemporaryPassword_PasswordStartingWithSpaceIsNotValid(t *testing.T) {
-	testFunc := users.GetTemporaryPassword
-	emailMessage := "<a href=\"https://sandbox.digital-services.skf.com/sign-in?user_name=contracts_gherkin-xy-zw@disposable-emails.enlight.skf.com&password= hopelessly-premium-eft\" class=\"button primary-button\">"
-
-	_, err := testFunc(emailMessage)
-
-	require.NotNil(t, err)
-}
-
 func TestGetTemporaryPassword_HyperLinkMissingAmpersandShouldError(t *testing.T) {
 	testFunc := users.GetTemporaryPassword
 	emailMessage := "<a href=\"https://sandbox.digital-services.skf.com/sign-in?user_name=contracts_gherkin-xy-zw@disposable-emails.enlight.skf.com%20password=hopelessly-premium-eft\" class=\"button primary-button\">"
